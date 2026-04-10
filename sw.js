47181681124bbc4b2dfb7308b1f58b6e73cf956a
Mayko <mayko@overlabs.app>
2026-04-10 19:12:52 -0300

    v46 - Atualizacao do app

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v45";
+const CACHE_NAME = "overlabs-v46";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
