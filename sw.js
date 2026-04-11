81018bc89dae93d7c03a78e91e4af7fd6946b260
Mayko <mayko@overlabs.app>
2026-04-11 08:47:00 -0300

    v90 - Fix editor: base64 em Firestore ao inves de Storage

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v89";
+const CACHE_NAME = "overlabs-v90";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
