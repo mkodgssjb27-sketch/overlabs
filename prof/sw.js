ca7eca0aeb0227bf09025e3ece6622bd02ebff94
Mayko <mayko@overlabs.app>
2026-04-11 13:11:37 -0300

    v127 - Sidebar: foto perfil 200px (2x original)

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v52";
+const CACHE_NAME_PROF = "overhost-v53";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v126";
+const CACHE_NAME = "overlabs-v127";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
