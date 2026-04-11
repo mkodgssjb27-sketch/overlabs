0a0e144660a14dd0d62597ec5bd2194bf41f55fc
Mayko <mayko@overlabs.app>
2026-04-11 16:24:29 -0300

    v146 - Sidebar: banner animado GIF carrega chunks completos e reproduz em looping

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v71";
+const CACHE_NAME_PROF = "overhost-v72";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v145";
+const CACHE_NAME = "overlabs-v146";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
