eb7abccab0a1ff1bca1893622da4e8f1d9f33361
Mayko <mayko@overlabs.app>
2026-04-11 13:32:56 -0300

    v132 - Force SW update - Decoracoes ja removido

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v57";
+const CACHE_NAME_PROF = "overhost-v58";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v131";
+const CACHE_NAME = "overlabs-v132";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
