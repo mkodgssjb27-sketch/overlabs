08313c10757448442a9f2089fe57e1012a22779a
Mayko <mayko@overlabs.app>
2026-04-11 00:11:08 -0300

    v82 - Pagina de instalacao dedicada prof/reset.html

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v8";
+const CACHE_NAME_PROF = "overhost-v9";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v81";
+const CACHE_NAME = "overlabs-v82";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
