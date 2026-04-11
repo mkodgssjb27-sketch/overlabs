35e7f713075fca23e38ccf26cf8b7dcd8fc1aa52
Mayko <mayko@overlabs.app>
2026-04-11 13:31:18 -0300

    v131 - Remove botao Decoracoes do dashboard professor

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v56";
+const CACHE_NAME_PROF = "overhost-v57";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v130";
+const CACHE_NAME = "overlabs-v131";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
