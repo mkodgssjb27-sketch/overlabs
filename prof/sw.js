5388a0cc2b0ccda3d53329a3473810b08f97ac19
Mayko <mayko@overlabs.app>
2026-04-11 00:27:12 -0300

    v87 - Pagina fix.html - guia passo a passo para separar os dois apps

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v13";
+const CACHE_NAME_PROF = "overhost-v14";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v86";
+const CACHE_NAME = "overlabs-v87";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
