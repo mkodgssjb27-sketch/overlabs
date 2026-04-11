2e7afea483bac5a4c0f18f799612199289c89b63
Mayko <mayko@overlabs.app>
2026-04-11 09:12:30 -0300

    v92 - Remove botao inventario sidebar, inventario fica dentro da loja

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v91";
+const CACHE_NAME = "overlabs-v92";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
