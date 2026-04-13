73cf9c6734ec56de2b891a302cd4f61e50c6db73
Mayko <mayko@overlabs.app>
2026-04-13 00:26:17 -0300

    v171 - editor: moldura tamanho max 300%

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v170";
+const CACHE_NAME = "overlabs-v171";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
