573015df80ee13b464f78a5685fbfacf6056ef4d
Mayko <mayko@overlabs.app>
2026-04-13 10:09:56 -0300

    v180 - Remove CUTS do card perfil ranking, centraliza emblemas

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v105";
+const CACHE_NAME_PROF = "overhost-v106";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v179";
+const CACHE_NAME = "overlabs-v180";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
