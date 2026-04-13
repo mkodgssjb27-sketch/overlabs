3596c2a3d9000008ef0b72b3a63f429c2c6b446b
Mayko <mayko@overlabs.app>
2026-04-13 12:46:40 -0300

    v187 - Fix toggle login diario - label clicavel

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v112";
+const CACHE_NAME_PROF = "overhost-v113";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v186";
+const CACHE_NAME = "overlabs-v187";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
