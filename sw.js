8a86616a8139e92d557d2421cd93eff7e8f12d6d
Mayko <mayko@overlabs.app>
2026-04-13 10:14:29 -0300

    v181 - Preload mercado em segundo plano ao abrir app

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v180";
+const CACHE_NAME = "overlabs-v181";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
