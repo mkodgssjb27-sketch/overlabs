c81c23ebddb3fbf473ea406ecf5956e1a9ce6a74
Mayko <mayko@overlabs.app>
2026-04-13 12:57:08 -0300

    v189 - Pull-to-refresh no log do professor

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v114";
+const CACHE_NAME_PROF = "overhost-v115";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v188";
+const CACHE_NAME = "overlabs-v189";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
