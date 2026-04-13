65d49b73dac55eca702358307108c6e682fce75e
Mayko <mayko@overlabs.app>
2026-04-13 19:38:59 -0300

    v194 - Renomeia Pega LAMPRA para Pega LAMPRAS

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v193";
+const CACHE_NAME = "overlabs-v194";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
