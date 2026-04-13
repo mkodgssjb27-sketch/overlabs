4f1d0b9366e0fba3c43fcde3e481000101e76369
Mayko <mayko@overlabs.app>
2026-04-13 20:06:48 -0300

    v198 - Pega LAMPRAS: boost segue dedo exato via delta tracking, mantido ate soltar

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v123";
+const CACHE_NAME_PROF = "overhost-v124";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v197";
+const CACHE_NAME = "overlabs-v198";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
