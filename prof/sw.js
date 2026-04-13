7387a3198e3aa18be01bdee70c33df5153c32fa0
Mayko <mayko@overlabs.app>
2026-04-13 19:58:07 -0300

    v197 - Remove botao X do container do jogo (usa Sair interno)

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v122";
+const CACHE_NAME_PROF = "overhost-v123";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v196";
+const CACHE_NAME = "overlabs-v197";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
