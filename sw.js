d593064ee720f724a55e841c0c32c90caecfe56e
Mayko <mayko@overlabs.app>
2026-04-13 10:02:01 -0300

    v179 - Ranking hibrido - CUTS decidem posicao, ordem manual so vale em empate

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v178";
+const CACHE_NAME = "overlabs-v179";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
