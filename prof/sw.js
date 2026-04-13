3f03bda015ff39c7eb71f23a249722ac8ebf015d
Mayko <mayko@overlabs.app>
2026-04-13 19:37:47 -0300

    v193 - Pega LAMPRA v2: HUD com Aura, timer 15min, sair com confirmacao, spectator mode, controle tap/drag/precisao, podio, voltar home

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v118";
+const CACHE_NAME_PROF = "overhost-v119";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v192";
+const CACHE_NAME = "overlabs-v193";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
