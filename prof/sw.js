4535d2c1863b865ef70a844f67e8063f4a1e39b8
Mayko <mayko@overlabs.app>
2026-04-13 21:25:43 -0300

    v204 - v204: min players config, max size 3x (240)

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v129";
+const CACHE_NAME_PROF = "overhost-v130";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v203";
+const CACHE_NAME = "overlabs-v204";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
