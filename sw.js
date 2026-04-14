92602aacbc188da95bebd5fc8bc25121f7552f6c
Mayko <mayko@overlabs.app>
2026-04-14 13:53:19 -0300

    v210 - Prof: login diario toggle - marcar e desmarcar dia do aluno

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v209";
+const CACHE_NAME = "overlabs-v210";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
