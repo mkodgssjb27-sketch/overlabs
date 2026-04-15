3b17de9cd65fb339ecb607d8932441068296c807
Mayko <mayko@overlabs.app>
2026-04-15 14:14:57 -0300

    v233 - Fix PTR log - t-content agora e scroll container real com overflow-y auto

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v158";
+const CACHE_NAME_PROF = "overhost-v159";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v232";
+const CACHE_NAME = "overlabs-v233";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
