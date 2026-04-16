b25d92ceb0625a348696a879bdc273b04492ef34
Mayko <mayko@overlabs.app>
2026-04-16 10:43:00 -0300

    v236 - Adiciona jogo Salto PAVOROSO com config no professor

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v161";
+const CACHE_NAME_PROF = "overhost-v162";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v235";
+const CACHE_NAME = "overlabs-v236";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
