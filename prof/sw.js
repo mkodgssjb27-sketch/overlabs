f628774535584feeac0631e8755a747a88c04b03
Mayko <mayko@overlabs.app>
2026-04-16 12:45:22 -0300

    v245 - Remove fullscreen lock que travava iframe - fix app travando

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v170";
+const CACHE_NAME_PROF = "overhost-v171";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v244";
+const CACHE_NAME = "overlabs-v245";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
