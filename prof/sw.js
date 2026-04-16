b0c0430a1cc9df79befb66598b55c0b23b4e531a
Mayko <mayko@overlabs.app>
2026-04-16 12:42:36 -0300

    v244 - Fullscreen landscape ao jogar + tela COMECAR antes de iniciar

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v169";
+const CACHE_NAME_PROF = "overhost-v170";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v243";
+const CACHE_NAME = "overlabs-v244";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
