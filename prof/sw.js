0b33ea80834f6c207d498b787cdda998087fa86d
Mayko <mayko@overlabs.app>
2026-05-03 20:03:54 -0300

    v253 - Reducao de vagas move ultimos confirmados para topo da fila

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v178";
+const CACHE_NAME_PROF = "overhost-v179";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v252";
+const CACHE_NAME = "overlabs-v253";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
