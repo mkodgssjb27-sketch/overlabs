a86aa6b17106828029197e74b1af8663d63cabdd
Mayko <mayko@overlabs.app>
2026-05-04 16:06:06 -0300

    v276 - debug: mostrar erro real ao listar torneios

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v275";
+const CACHE_NAME = "overlabs-v276";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
