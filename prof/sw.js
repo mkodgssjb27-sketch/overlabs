f1fa5f0fa20231fc2b14632b23e6d19b7d70ea48
Mayko <mayko@overlabs.app>
2026-05-04 09:09:16 -0300

    v257 - Presencas: lancamento em massa com selecao de alunos

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v182";
+const CACHE_NAME_PROF = "overhost-v183";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v256";
+const CACHE_NAME = "overlabs-v257";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
