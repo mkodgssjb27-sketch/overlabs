ab00cb661c68b092d54d47c32ea481b1ecc546a6
Mayko <mayko@overlabs.app>
2026-05-04 18:53:24 -0300

    v279 - Adicionar equipe manualmente em inscricao

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v204";
+const CACHE_NAME_PROF = "overhost-v205";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v278";
+const CACHE_NAME = "overlabs-v279";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
