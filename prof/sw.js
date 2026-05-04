6df9d2d55a5fd774082357bc4da78f3682641676
Mayko <mayko@overlabs.app>
2026-05-04 10:39:33 -0300

    v264 - feat: ranking mensal (monthlyCuts) - reset auto dia 1 + botao manual no prof

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v189";
+const CACHE_NAME_PROF = "overhost-v190";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v263";
+const CACHE_NAME = "overlabs-v264";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
