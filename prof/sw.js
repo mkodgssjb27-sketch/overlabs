6958c83b509f327fc8b69286851ee767ff587530
Mayko <mayko@overlabs.app>
2026-05-05 10:08:15 -0300

    v286 - Aluno: ranking profile - carteira compacta no canto esquerdo

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v211";
+const CACHE_NAME_PROF = "overhost-v212";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v285";
+const CACHE_NAME = "overlabs-v286";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
