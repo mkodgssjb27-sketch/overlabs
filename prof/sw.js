ff888146984b68d2313da3706dd7b3e35784d9a6
Mayko <mayko@overlabs.app>
2026-05-05 14:18:52 -0300

    v291 - Editor: video tambem para Avatar; preview do recorte em loop dentro da janela

--- prof/sw.js
+++ prof/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME_PROF = "overhost-v216";
+const CACHE_NAME_PROF = "overhost-v217";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v290";
+const CACHE_NAME = "overlabs-v291";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
