fc66d06f8b35e0e42c6ea6603695189f0983bced
Mayko <mayko@overlabs.app>
2026-05-05 14:31:16 -0300

    v292 - Editor: filtros Ativos/Expirados/Todos na lista de itens

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v291";
+const CACHE_NAME = "overlabs-v292";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
