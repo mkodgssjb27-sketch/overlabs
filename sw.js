0523b5b7bc077e69f91c9d29e659a470c5c584a4
Mayko <mayko@overlabs.app>
2026-05-05 10:35:38 -0300

    v287 - Editor de Artes: importar abre galeria completa do dispositivo

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ messaging.onBackgroundMessage(payload => {
   return self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = "overlabs-v286";
+const CACHE_NAME = "overlabs-v287";
 const URLS_TO_CACHE = [
   "./aluno.html",
   "./manifest.json",
